fb6d07198920e13b29ffe0ccdef5d9cb4546bbfb
WitherTaco <obrushego@gmail.com>
2023-10-24 14:56:40 +0300

    Preparation paladin class

---
+++ PaladinDamageType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace WotTK
+{
+    public class PaladinDamageType : DamageClass
+    {
+        public override bool GetEffectInheritance(DamageClass damageClass)
+        {
+            if (damageClass == DamageClass.Melee)
+                return true;
+            return false;
+        }
+        public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+        {
+            if (damageClass == DamageClass.Generic || damageClass == DamageClass.Melee)
+                return StatInheritanceData.Full;
+            return StatInheritanceData.None;
+        }
+    }
+}
---
+++ WotTKPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace WotTK
+{
+    public class WotTKPlayer : ModPlayer
+    {
+        public bool hasPaladinTeamBuff;
+        public int paladinsTeam;
+        public override void ResetEffects()
+        {
+            paladinsTeam = -1;
+            hasPaladinTeamBuff = false;
+        }
+        public override void PostUpdateEquips()
+        {
+            if (hasPaladinTeamBuff)
+            {
+                paladinsTeam = Player.team;
+            }
+            for (int i = 0; i < Main.player.Length; i++)
+            {
+                Player plr = Main.player[i];
+                if (plr == null) continue;
+                if (!plr.active) continue;
+                if (plr.team == paladinsTeam && !Player.dead)
+                {
+                    //Effect for your team
+                }
+            }
+        }
+    }
+}
