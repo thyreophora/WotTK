137bc1de5afc5e029493f9678c844f9c738c99c5
thyreophora <124232605+thyreophora@users.noreply.github.com>
2023-10-29 01:45:34 -0700

    rare

--- Content/Items/Weapons/Melee/Mace/MaceofExpertise.cs
+++ Content/Items/Weapons/Melee/Mace/MaceofExpertise.cs
@@ -17,7 +17,7 @@ namespace WotTK.Content.Items.Weapons.Melee.Mace
         {
             Item.width = Item.height = 50;
             Item.value = 4000;
-            Item.rare = 0;
+            Item.rare = 3;
             Item.scale = 1.5f;
 
             Item.damage = 25;
