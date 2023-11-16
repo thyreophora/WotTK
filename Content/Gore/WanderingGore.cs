62b38998c3c93506c4b45b6227363cd684e60bf6
thyreophora <124232605+thyreophora@users.noreply.github.com>
2023-11-16 13:50:03 -0800

    Small Changes

---
+++ Content/Gore/WanderingGore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace WotTK.Content.Gore
+{
+    public class WanderingGore : ModGore
+    {
+        public class EyeGore1 : ModGore
+        {
+            public class EyeGore2 : ModGore
+            {
+
+            }
+        }
+    }
+}
