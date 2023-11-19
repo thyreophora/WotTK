007f53c9a25f4de371b81c5ecc2624cbd4f77aa1
WitherTaco <sharonovmisha2005@outlook.com>
2023-11-19 12:37:06 +0300

    aftermerge changes 2

--- Content/Items/Materials/WoolCloth.cs
+++ Content/Items/Materials/WoolCloth.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace WotTK.Content.Materials
+namespace WotTK.Content.Items.Materials
 {
     public class WoolCloth : ModItem
     {
