d8111059ce2a8c5747d732afa47eed595ccf8711
thyreophora <124232605+thyreophora@users.noreply.github.com>
2023-12-07 10:16:41 -0800

    BlackholeWand 2/3

--- Content/Items/Weapons/Magic/Staffs/BlackholeWand.cs
+++ Content/Items/Weapons/Magic/Staffs/BlackholeWand.cs
@@ -1,5 +1,7 @@
 using Microsoft.Xna.Framework;
 using WotTK.Content.Items.Materials;
+using Terraria.Audio;
+using WotTK.Utilities;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -7,37 +9,31 @@ using Terraria.ModLoader;
 
 namespace WotTK.Content.Items.Weapons.Magic.Staffs
 {
-    public class BlackholeWand : ModItem
+    public class BlackholeWand : LevelLockedItem
     {
-        public override void SetStaticDefaults()
-        {
-            /* Tooltip.SetDefault("Fires an arc of digestive fluid at the enemy\n" +
-                "Inflicts a defense-reducing stomach acid debuff"); */
-            Item.staff[Item.type] = true;
-
-            Item.ResearchUnlockCount = 1;
-        }
-
+        public override int MinimalLevel => 32;
+        public override bool IsWeapon => true;
         public override void SetDefaults()
         {
-            Item.damage = 57;
-            Item.DamageType = DamageClass.Magic;
-            Item.mana = 20;
-            Item.width = 28;
-            Item.height = 28;
-            Item.useTime = 6;
-            Item.useAnimation = 24;
-            Item.reuseDelay = 30;
+            Item.width = 86;
+            Item.height = 44;
+            Item.value = 10000;
+            Item.rare = ItemRarityID.Orange;
+
+            Item.useTime = Item.useAnimation = 20;
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.noMelee = true;
+            Item.UseSound = new SoundStyle("WotTK/Sounds/Custom/WandBaseSound");
             Item.autoReuse = true;
-            Item.knockBack = 4;
-            Item.value = Item.sellPrice(0, 2, 0, 0);
-            Item.rare = ItemRarityID.LightRed;
-            Item.shootSpeed = 19;
+
+            Item.mana = 10;
+            Item.damage = 50;
+            Item.knockBack = 2f;
+            Item.DamageType = DamageClass.Magic;
+
             Item.shoot = ModContent.ProjectileType<BlackholeWand_Proj>();
-            Item.UseSound = SoundID.NPCHit20;
+            Item.shootSpeed = 20;
         }
+
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
             int numberProjectiles = 2;
