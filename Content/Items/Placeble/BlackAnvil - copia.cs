7affc181271cd9227df84233fd7c1529e6630d37
thyreophora <vale-05@outlook.com>
2024-03-21 21:19:02 -0700

    LeatherworkingTable

---
+++ Content/Items/Placeble/BlackAnvil - copia.cs
@@ -0,0 +1,77 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System.Collections.Generic;
+using Terraria.Localization;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+using WotTK.Content.Items.Materials;
+using System;
+using WotTK.Common;
+
+namespace WotTK.Content.Items.Placeble
+{
+    public class LeatherworkingTable : LevelLockedItem
+    {
+        public override int MinimalLevel => 5;
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 38;
+            Item.height = 20;
+            Item.maxStack = 99;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = 1;
+            Item.consumable = true;
+            Item.rare = -11;
+            Item.value = Item.sellPrice(0, 5, 0, 0);
+            Item.createTile = ModContent.TileType<LeatherworkingTile>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<OrdilWood>(), 12)
+                .AddIngredient(ModContent.ItemType<ThicLeather>(), 4)
+                .AddIngredient(ItemID.Sawmill)
+                .AddTile(TileID.Anvils)
+                .AddCondition(LevelLockedRecipe.ConstructRecipeCondition(MinimalLevel, out Func<bool> condition), condition)
+                .Register();
+
+        }
+    }
+    public class LeatherworkingTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileLighted[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = false;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style5x4); // Uses 5x4 style, but reduces height to 3.
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 18 };
+            TileObjectData.newTile.Origin = new Point16(2, 1);
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(60, 36, 27),
+            Language.GetText("Leatherworking Table"));
+            TileID.Sets.DisableSmartCursor[Type] = true;
+
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            DustType = DustID.DynastyWood; // 207
+
+
+            }
+        }
+    }
+}
